52e424b9484cea58fe2949bb8072799790d8378c
sfandrw <kclgkt@gmail.com>
2026-09-10 21:05:26 +0100

    Revert "Update parent dashboard and add parent homework planner"
    This reverts commit f34d31a19c4f293e43eb158c512ca5eea55aeb26.

--- practice/urls.py
+++ practice/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from . import parent_views, views
+from . import views
 
 app_name = "practice"
 
 urlpatterns = [
-    path("dashboard/", parent_views.dashboard, name="dashboard"),
-    path("parent/", parent_views.parent_dashboard, name="parent_dashboard"),
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("practice/", views.choose, name="choose"),
     path("practice/subject/<str:code>/", views.subject_detail, name="subject_detail"),
     path("practice/start/<int:subtopic_id>/", views.start, name="start"),
--- school_onboarding/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolOnboardingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "school_onboarding"
-    verbose_name = "School onboarding"
